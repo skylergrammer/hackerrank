b091dfaf4b26102972d2a16255d313e3df571bfc
sgrammer <skylergrammer@gmail.com>
2015-08-24 13:51:58 -0500

    pos

--- pacman-dfs.py
+++ pacman-dfs.py
@@ -2,65 +2,60 @@ import numpy as np
 
 def dfs(r, c, pacman_r, pacman_c, food_r, food_c, grid):
     grid = np.matrix([list(x) for x in grid])
-    col_num = pacman_c
-    row_num = pacman_r
-    visited = []
 
-    contents = grid.item(row_num, col_num)
+    visited, stack = [], [(pacman_r, pacman_c)]
+    moves = []
+    vertex = (pacman_r, pacman_c)
+    while len(stack) > 0 and vertex != (food_r, food_c):
+        vertex = stack.pop()
+        if vertex in visited:
+            continue
 
-    n = 0
-    while contents != '.' and n < 20:
-        contents = grid.item(row_num, col_num)
-        visited.append((row_num, col_num))
+        visited.append(vertex)
+        moves.append(vertex)
 
-        if row_num-1 >= 0:
-            contents_up = grid.item(row_num-1, col_num)
-        else:
-            contents_up = None
+        row, col = vertex
+        up_c = (row-1, col)
+        left_c = (row, col-1)
+        right_c = (row, col+1)
+        down_c = (row+1, col)
 
-        if col_num-1 >= 0:
-            contents_left = grid.item(row_num, col_num-1)
-        else:
-            contents_left = None
+        for each in [up_c, left_c, right_c, down_c]:
+            xx, yy = each
+            try:
+                box = grid.item(each)
+                if 0 <= xx <= r-1 and 0 <= yy <= c-1 and box != '%':
+                    stack.append(each)
+            except:
+                continue
+                
+    return moves
 
-        if col_num+1 <= c-1:
-            contents_right = grid.item(row_num, col_num+1)
-        else:
-            contents_right = None
-
-        if row_num+1 <= r-1:
-            contents_down = grid.item(row_num-1, col_num)
-        else:
-            contents_down = None
-
-        if contents_up and contents_up == '-' and (row_num-1, col_num) not in visited:
-            row_num -= 1
-        elif contents_left and contents_left == '-' and (row_num, col_num-1) not in visited:
-            col_num -= 1
-        elif contents_right and contents_right == '-' and (row_num+1, col_num) not in visited:
-            row_num += 1
-        elif contents_down and contents_down == '-' and (row_num, col_num-1) not in visited:
-            col_num += 1
-
-        n+=1
-
-        print row_num, col_num
-
-    return
 
 def main():
 
-    grid = ['%%%%%%%%%%%%%%%%%%%%',
+    grid = [
+            '%%%%%%%%%%%%%%%%%%%%',
             '%--------------%---%',
             '%-%%-%%-%%-%%-%%-%-%',
             '%--------P-------%-%',
             '%%%%%%%%%%%%%%%%%%-%',
             '%.-----------------%',
-            '%%%%%%%%%%%%%%%%%%%%']
-    pacman_r, pacman_c = 3, 9
-    food_c, food_r = 5, 1
+            '%%%%%%%%%%%%%%%%%%%%'
+            ]
+    pacman_r, pacman_c = 3,9
+    food_r, food_c = 5, 1
     r, c = 7, 20
-    new_grid = dfs(r, c, pacman_r, pacman_c, food_r, food_c, grid)
+    moves = dfs(r, c, pacman_r, pacman_c, food_r, food_c, grid)
 
+    print len(moves)
+    for each in moves:
+        rr, cc = each
+        print rr, cc
+
+    print len(moves)-1
+    for each in moves:
+        rr, cc = each
+        print rr, cc
 if __name__ == "__main__":
     main()
